d0fa2186e9f7dbde6d12446bde6ba6cccd4864de
Lucien40 <huberlulu@gmail.com>
2018-03-25 22:51:02 +0200

    Self operators

--- vectors/Vecteur.cpp
+++ vectors/Vecteur.cpp
@@ -11,6 +11,22 @@ void Vecteur::augmente(double d){
   dimension++;
 }
 
+Vecteur::Vecteur(unsigned int dim) {
+    dimension = dim;
+    data = vector(dim);
+}
+
+Vecteur::Vecteur(initializer_list<double> const& list) {
+    for (auto a : list){
+        this.augmente(d);
+    }
+}
+
+Vecteur::Vecteur(double x, double y, double z) {
+    dimension =3;
+    data = vector({x,y,z});
+}
+
 void Vecteur::setCoord(size_t i, double d){
   try{
     if(i < dimension) {
@@ -24,7 +40,7 @@ void Vecteur::setCoord(size_t i, double d){
 std::string Vecteur::toString() const{
   std::string s;
   for (auto& d : data) {
-    s += std::to_string(d) + ' '; // TODO too mant zeros after decimal
+    s += to_string(d) + ' '; // TODO too many zeros after decimal
   }
   return s;
 }
@@ -50,6 +66,48 @@ bool Vecteur::operator==(Vecteur const& v) const{
     if(v.get(i) != data[i]) return false;
   }
   return true;
+}
+void Vecteur::set(unsigned int i, double d){
+    data[i] = d;
+}
+
+Vecteur& Vecteur::operator+=(Vecteur const & vAdded){
+    try {
+        if(dimension == vAdded.getDimension()) throw out_of_range ("mismatched dimensions");
+        for (unsigned int i = 0; i < dimension; ++i) {
+            data[i] += vAdded.get(i);
+        }
+    }catch (const std::out_of_range& oor){
+        cerr << "Out of range error: " << oor.what() << endl;
+    }
+    return *this;
+}
+
+const Vecteur Vecteur::operator-() const{
+    Vecteur retour(dimension);
+    for(unsigned int i(0); i < dimension; ++i){
+        retour.setCoord(i,data[i]);
+    }
+    return retour;
+}
+
+Vecteur& Vecteur::operator-=(Vecteur const &){
+    try {
+        if(dimension == vAdded.getDimension()) throw out_of_range ("mismatched dimensions");
+        for (unsigned int i = 0; i < dimension; ++i) {
+            data[i] -= vAdded.get(i);
+        }
+    }catch (const std::out_of_range& oor){
+        cerr << "Out of range error: " << oor.what() << endl;
+    }
+    return *this;
+}
+
+Vecteur& Vecteur::operator*=(double d){
+    for (unsigned int i = 0; i < dimension; ++i) {
+        data[i] *= d;
+    }
+    return *this;
 }
 
 std::ostream& operator<<(std::ostream& output, Vecteur const& v){
--- vectors/Vecteur.h
+++ vectors/Vecteur.h
@@ -1,5 +1,6 @@
 #ifndef Vecteur_h
 #define Vecteur_h
+
 #include <vector>
 #include <string>
 #include <iostream>
@@ -8,24 +9,46 @@
 class Vecteur {
 private:
 /* data */
-unsigned int dimension = 0;
-std::vector<double> data;
+    unsigned int dimension = 0;
+    std::vector<double> data;
 
 public:
-void augmente(double d);
 
-void setCoord(std::size_t i, double d);
+    //Constructors
 
-unsigned int getDimension() const;
+    Vecteur(initializer_list<double> const& );
 
-double get(unsigned int i) const;
+    Vecteur(unsigned int);
 
-bool operator==(Vecteur const&) const;
+    Vecteur(double x,double y,double z);
 
-std::string toString() const;
+    void augmente(double d);
+
+    void setCoord(std::size_t i, double d);
+
+    unsigned int getDimension() const;
+
+    double get(unsigned int i) const;
+
+
+
+    bool operator==(Vecteur const &) const;
+
+    Vecteur& operator+=(Vecteur const &);  // returns a vecteur reference because operation has to have value (thus reference)
+
+    const Vecteur operator-() const;
+
+    Vecteur& operator-=(Vecteur const &);
+
+    Vecteur& operator*=(double);
+
+    std::string toString() const;
 
 };
 
-std::ostream& operator<<(std::ostream&, Vecteur const&);
+std::ostream& operator<<(std::ostream &, Vecteur const &);
+
+double operator^(Vecteur const& , Vecteur const&);
+
 
 #endif
